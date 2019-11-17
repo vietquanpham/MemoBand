f18e6eb7fddf87f20e0d1c2943be6d9ffd79f030
vietquanpham <quanhuongoanh@gmail.com>
2019-11-16 22:47:08 -0500

    added medication

--- app.py
+++ app.py
@@ -15,6 +15,33 @@ db = DatabaseConnection()
 def index(): 
     return render_template("index.html")
 
+@app.route("/medication", methods=["GET", "POST"])
+def medication():
+    if request.method == 'POST':
+        document = {
+            "label": request.form['label'],
+            "day": request.form.getlist('day[]'),
+            "location": request.form['location'],
+            "notes": request.form['notes'],
+        }
+        db.insert("medication", document)
+        return render_template("medication.html")
+    else:
+        return render_template("medication.html")
+
+@app.route("/meal", methods=["GET", "POST"])
+def meal():
+    if request.method == 'POST':
+        document = {
+            "meal": request.form['meal'],
+            "time": request.form['time'],
+            "notes": request.form['notes'],
+        }
+        db.insert("meal", document)
+        return render_template("meal.html")
+    else:
+        return render_template("meal.html")
+
 if __name__ == "__main__":
     app.secret_key = "secretkey"
     app.run(host="localhost", port=4000, debug=True)
